e52b3c10de11a7552b7af86f84341954c2ea5308
amhayslip <amhayslip@gmail.com>
2020-11-08 14:36:41 -0500

    update for storing on aws and getting back at html

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
-const TurndownService = require('turndown');
-
+const AWS = require('aws-sdk');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -11,18 +9,35 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const saveFile = (md, title, res) => {
-  fs.writeFile(`${__dirname}/files/${title}.md`, md, err => {
-    if (err) throw err;
-    console.log('The file has been saved!');
+require('dotenv').config();
 
-    res.send('done!');
-  });
-};
+const ID = process.env.AWS_ACCESS_KEY_ID;
+const SECRET = process.env.AWS_SECRET_ACCESS_KEY;
+const BUCKET_NAME = process.env.AWS_BUCKET;
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+const s3 = new AWS.S3({
+  region: 'us-east-1',
+  accessKeyId: ID,
+  secretAccessKey: SECRET,
 });
+
+const uploadFile = (content, title, res) => {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: `${title}.html`, // File name you want to save as in S3
+    Body: content,
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+
+  res.send('great success!');
+};
 
 app.post('/addfile', function(req, res) {
   const { content } = req.body;
@@ -33,10 +48,60 @@ app.post('/addfile', function(req, res) {
     .join('-')
     .toLowerCase();
 
-  const turndownService = new TurndownService();
-  const markdown = turndownService.turndown(content);
+  uploadFile(content, fileSafeTitle, res);
+});
 
-  saveFile(markdown, fileSafeTitle, res);
+app.get('/getFiles', function(req, res) {
+  const getFile = function(key, callback) {
+    s3.getObject({ Bucket: 'weekly-project-shift-emails', Key: key }, function(error, data) {
+      callback({
+        content: data.Body.toString('utf8'),
+        title: key,
+      });
+
+      if (error != null) {
+        console.log(`Failed to retrieve an object: ${error}`);
+      } else {
+        console.log(`Loaded ${data.ContentLength} bytes`);
+      }
+    });
+  };
+
+  async function listAllObjectsFromS3Bucket(bucket, prefix) {
+    let isTruncated = true;
+    let marker;
+    while (isTruncated) {
+      const params = { Bucket: bucket };
+      if (prefix) params.Prefix = prefix;
+      if (marker) params.Marker = marker;
+      try {
+        const response = await s3.listObjects(params).promise();
+        const myPromises = [];
+        response.Contents.forEach(item => {
+          myPromises.push(
+            new Promise((resolve, reject) => {
+              getFile(item.Key, function(data) {
+                resolve(data);
+              });
+            })
+          );
+        });
+
+        Promise.all(myPromises).then(values => {
+          res.send(values);
+        });
+
+        isTruncated = response.IsTruncated;
+        if (isTruncated) {
+          marker = response.Contents.slice(-1)[0].Key;
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+
+  listAllObjectsFromS3Bucket('weekly-project-shift-emails');
 });
 
 const port = process.env.PORT || 5000;
